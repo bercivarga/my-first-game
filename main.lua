a250101360df6509d9bc888565ead3d9a7f1f529
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-01-23 23:18:44 +0100

    Add game over and timer

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ function love.load()
   target.radius = 50
 
   score = 0
-  timer = 0
+  timer = 5
 
   gameFont = love.graphics.newFont(40)
 
@@ -13,23 +13,41 @@ function love.load()
   circleRGB.r = 1
   circleRGB.g = 1
   circleRGB.b = 1
+
+  gameLive = true
+
+  function resetGame()
+    score = 0
+    timer = 5
+  end
 end
 
 function love.update(dt)
-
+  if timer > 0 then
+    timer = timer - dt
+  end
+  if timer < 0 then
+    timer = 0
+  end
 end
 
 function love.draw()
-  love.graphics.setColor(circleRGB.r, circleRGB.g, circleRGB.b)
-  love.graphics.circle("fill", target.x, target.y, target.radius)
-  
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.setFont(gameFont)
-  love.graphics.print(score, 0, 0)
+  if timer > 0 then
+    love.graphics.setColor(circleRGB.r, circleRGB.g, circleRGB.b)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print("Score: "..score, 20, 20)
+    love.graphics.print("Time left: "..math.ceil(timer), 400, 20)
+  end
+
+  if timer == 0 then
+    love.graphics.print("Game over, final score: "..score, 20, love.graphics.getHeight() - 80)
+  end
 end
 
-function love.mousepressed(x, y, button, istouch, presses)
-  if button == 1 then
+function love.mousepressed(x, y, button)
+  if button == 1 and timer > 0 then
     local mouseToTarget = distanceBetween(x, y, target.x, target.y)
     if mouseToTarget < target.radius then
       score = score + 1
@@ -41,10 +59,11 @@ function love.mousepressed(x, y, button, istouch, presses)
       circleRGB.r = math.random()
       circleRGB.g = math.random()
       circleRGB.b = math.random()
-
     else
       score = 0
     end
+    else
+      resetGame()
   end
 end
 
